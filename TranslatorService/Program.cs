aec81a9215c0fd3158af0277aae3ba381e3d8f98
zigfried123 <zigfried123@gmail.com>
2025-10-12 21:10:22 +0400

    added translator service in docker

---
+++ TranslatorService/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/translator-list", () =>
+{
+    bool isListReady = false;
+
+    string message;
+
+    if(isListReady){
+        message = "Список переводчиков готов";
+    }else{
+        message = "Нет свободных переводчиков";
+    }
+
+    return message;
+})
+.WithName("GetTranslatorList");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
